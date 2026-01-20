814d35e065a7bb03ebb5df4ac3e500094b438268
ethannCB <ethann.campionbaux@gmail.com>
2026-01-20 09:35:25 +0100

    First commit

---
+++ level.c
@@ -0,0 +1,17 @@
+
+int min(int a , int b , int c){
+    int m=a;
+    if(b<m) m=b;
+    if(c<m) m=c;
+    return m;
+}
+// """
+// int levenshtein(char *s1, char *s2) {
+//     int taille1 = 0;
+//     int taille2 = 0;
+//     while(char *s1++) taille1++;
+//     while(char *s2++) taille2++;
+//     int d[taille1+1][taille2+1];
+//     return 0;
+// }
+// """
